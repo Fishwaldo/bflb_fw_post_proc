91afa0110fe3207985a0caade67a9b28a3e9de2c
Justin Hammond <justin@dynam.ac>
2023-02-26 23:00:56 +0800

    Works with bflb_mcu_tool

--- libs/bl808/chiptype_patch.py
+++
@@ -1,9 +0,0 @@
-# uncompyle6 version 3.9.0
-# Python bytecode version base 3.7.0 (3394)
-# Decompiled from: Python 3.8.10 (default, Nov 14 2022, 12:59:47) 
-# [GCC 9.4.0]
-# Embedded file name: libs/bl808/chiptype_patch.py
-
-
-def img_load_create_predata_before_run_img():
-    return bytearray(0)
